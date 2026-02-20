2497c054fa6f1cc5e10a2f22f59843da80e31b71
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2026-02-20 17:13:38 +0800

    chore(consolidation): Consolidate two repos

---
+++ session-01/mary-had-a-alittle-lamb.py
@@ -0,0 +1,16 @@
+# Mary had a Litte Lamb
+#
+# File: ICTPRG302-2026-S1/Session-01/mary-had-a-little-lamb.py
+# Author: Adrian Gould
+# Copyright: Copyright 2026, Adrian Gould
+
+print("Mary Had a Little Lamb")
+print()
+print("Mary had a little lamb,")
+print("Little lamb, little lamb,")
+print("Mary had a little lamb,")
+print("Its fleece was white as snow.")
+print("And everywhere that Mary went,")
+print("Mary went, Mary went,")
+print("Everywhere that Mary went,")
+print("The lamb was sure to go.")
