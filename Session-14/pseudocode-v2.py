8bc61cbc1dc8b510b0da412006d72af62a763f11
A gould <adrian.gould@nmtafe.wa.edu.au>
2026-05-15 14:22:41 +0800

    wip: pseudocode

---
+++ Session-14/pseudocode-v2.py
@@ -0,0 +1,85 @@
+# ASSESSMENT_TITLE
+#
+# Note: Base Pseudocode Version 1
+#
+# Author: YOUR_NAME
+# Student ID: YOUR_STUDENT_ID
+#
+# Course: YOUR_COURSE
+# Lecturer: YOUR_LECTURER_NAME
+
+# TODO: Add Import statements (if needed)
+
+# Variables and Constants
+# TODO: Define Constants
+DEBUG = True
+
+# TODO: Define Variables
+
+# Application Functions
+# TODO: Write Score Guess Function
+def score_guess():
+    # TODO: Write score guess code
+    print("Score guess code not ready")
+
+# TODO: Write Read File Into Word List Function
+def read_file_into_list():
+    # TODO: write code to read file into list
+    print("Read file into list code not ready")
+
+# TODO: Write Display Greeting Function
+def show_greeting():
+    print("Welcome")
+
+# TODO: Write Display Instructions Function
+def show_instructions():
+    print("Instructions")
+
+# TODO: Any Optional Additional Functions
+
+# TODO: Write get player name function
+
+# TODO: Write Ask player if they want instructions function
+
+
+# TODO: Play Game Function
+def play_game():
+    # TODO: Display Greeting
+
+    # TODO Get player's name
+
+    # TODO: Ask Player if they want instructions
+    # TODO: Does Player Want Instructions?
+    # TODO:     YES: Show instructions
+
+    # TODO: Read target words into target word list
+    # TODO: Read Allowed Words into Allowed word list
+
+    # TODO: Set target word to a random word from target word list
+
+    # TODO: For guesses in range 1 to 6
+    # TODO:     get Guess Word from user(allowed Word list
+    # TODO:     score word
+    # TODO:     Display score to player
+    # TODO:     If guesses is zero:
+    # TODO:         Break out of for loop
+
+    # TODO: Display End of Game Message
+    print("play the game code")
+
+#TODO: Testing Function
+def test_game():
+    print("Testing game code")
+
+    # Test 1
+    # Arrange
+    # Act
+    # Assert
+
+
+
+#TODO: Main Program
+if DEBUG:
+    test_game()
+else:
+    play_game()
